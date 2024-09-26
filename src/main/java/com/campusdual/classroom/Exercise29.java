1e9dcab05bfc05851f1d34306b8ece27999442f1
eluiui <jesusmiguez@gmail.com>
2024-09-26 14:51:46 +0200

    completado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            throw new IllegalArgumentException("Excepcion");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Problema evitado: " + e.getMessage());
+        }
     }
 }
